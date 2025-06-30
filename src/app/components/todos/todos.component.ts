44ab42df31eb303441c8a10caaefec657e3e9eb7
Yasin <ydehfuli@gmail.com>
2025-06-30 12:46:32 +0330

    feat(done-task): added done tasks and length of tasks in lists

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,20 +1,34 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TodoComponent} from '../todo/todo.component';
-import {NgForOf} from '@angular/common';
+import {NgClass, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-todos',
   imports: [
     TodoComponent,
-    NgForOf
+    NgForOf,
+    NgClass
   ],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent {
+  doneStatus: boolean = false
   @Input() todos: Array<{ key: number; done: boolean; text: string; }> = []
   @Output() delete = new EventEmitter<number>();
   @Output() status = new EventEmitter<number>();
+
+  get filterTodos() {
+    return this.todos.filter(item => item.done === this.doneStatus)
+  }
+
+  get doneTodos() {
+    return this.todos.filter(item => !item.done)
+  }
+
+  get unDoneTodos() {
+    return this.todos.filter(item => item.done)
+  }
 
   doDeleteTodo(key: number) {
     this.delete.emit(key)
